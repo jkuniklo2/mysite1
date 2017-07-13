a6646711dddd68732e900978d5b6c27090f4b980
Joe Kuniklo <jkuniklo2@gmail.com>
2017-07-13 10:20:37 +0100

    Add books app

---
+++ mysite1/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import Http404, HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render(request, "current_datetime.html", {'current_date': now})
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    return render(request, "hours_ahead.html", {'hour_offset': offset, 'next_time': dt})
